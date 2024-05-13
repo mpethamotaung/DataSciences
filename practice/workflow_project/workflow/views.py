4f4c38aa5fa3c37eb0f44c7f13ddc9b8bf61a093
Hanma-yujiro <mpethamotaung@gmail.com>
2024-05-13 19:17:54 +0200

    define views, handle HTTP req and render

--- practice/workflow_project/workflow/views.py
+++ practice/workflow_project/workflow/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Workflow
 
 # Create your views here.
+def workflow_list(request):
+    workflows = Workflow_objects.all()
+    return render(request, 'workflow/workflow_list.html', {'workflows': workflows})
