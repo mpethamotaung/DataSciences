b8f3445441e3dd906334f697cef287018df8d50c
Hanma-yujiro <mpethamotaung@gmail.com>
2024-05-13 19:36:19 +0200

    init django project

--- practice/workflow_project/workflow/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Workflow(models.Model):
-    name = models.CharField(max_lenghth=100)
-
-class Step(models.Model):
-    name = models.CharField(max_length=100)
-    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
--- practice/workflow_project/workflow/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Workflow
-
-# Create your views here.
-def workflow_list(request):
-    workflows = Workflow.objects.all()
-    return render(request, 'workflow/workflow_list.html', {'workflows': workflows})
