b9fc189e2ed0e192c6229b6819442e7086b59054
Hanma-yujiro <mpethamotaung@gmail.com>
2024-05-14 10:18:43 +0200

    basic index.html

--- workflow_system/customer/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.Charfield(max_length=100)
-    date_of_birth = models.DateField()
-    excel_file = models.FileField(upload_to='uploads/')
