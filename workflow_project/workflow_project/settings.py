a8aba8d2364f759f1ae6f6a6e6bc57aec4d05808
Hanma-yujiro <mpethamotaung@gmail.com>
2024-05-14 10:40:24 +0200

    init workflow project & app

--- workflow_project/workflow_project/settings.py
+++ workflow_project/workflow_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gnefb6y-z41&se7xbm*4@b!)nf(31hi55d_(v8(zu9m_&_^zox'
+SECRET_KEY = 'django-insecure-=9v7^m^vp1hc(xfi*)f93b#rskr1z^u^+e=#1uu19$zr4h5sl8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
